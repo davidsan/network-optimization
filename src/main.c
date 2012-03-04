9325bf7ce25f937083931af37b6df054146a4a0f
David San <davidsanfr@gmail.com>
2012-03-04 16:56:19 +0100

    xfigTools

--- src/main.c
+++ src/main.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 #include "fichier.h"
 #include "chaine.h"
+#include "xfigTools.h"
 
 int main(int argc, char **argv)
 {
-    FILE *f = NULL;
-    ListeChaine *L = creerListeChaine();
+  FILE *f = NULL;
+  FILE *fig = NULL;
+  fig = fopen("output.fig", "w");
+  if(!fig){
+    return 1;
+  }
+  ListeChaine *L = creerListeChaine();
 
-    if (L == NULL) {
-	leverErreur("création d'une ListeChaine vide");
-    }
+  if (L == NULL) {
+    leverErreur("création d'une ListeChaine vide");
+    return 1;
+  }
 
-    if (argc == 1) {		/* pas d'argument */
-	f = ouvrirFichier("../res/00014_burma.cha");
-    } else {
-	f = ouvrirFichier(argv[1]);
-    }
+  if (argc == 1) {		/* pas d'argument */
+    f = ouvrirFichier("../res/00014_burma.cha");
 
-    lecture_chaine(f, L);
-    displayListeChaine(stdout, L);
+  } else {
+    f = ouvrirFichier(argv[1]);
+  }
+  if(!f){
+    fclose(fig);
+    return 1;
+  }
 
-    fermerFichier(f);
-    freeListeChaine(L);
+  lecture_chaine(f, L);
+  /* displayListeChaine(stdout, L); */
 
-    return 0;
+  ecrit_entete_xfig(fig);
+  ecrit_chaine_xfig(L, fig, 1000, 1);
+
+  fermerFichier(f);
+  fermerFichier(fig);
+  freeListeChaine(L);
+    
+  return 0;
 }
---
+++ src/xfigTools.h
@@ -0,0 +1,9 @@
+#ifndef __XFIGTOOLS_H__
+#define __XFIGTOOLS_H__
+#include <stdio.h>
+#include "chaine.h"
+#include "erreur.h"
+void ecrit_entete_xfig(FILE *f);
+void ecrit_chaine_xfig(ListeChaine *L, FILE *f, int zoom, int epaisseur);
+
+#endif
